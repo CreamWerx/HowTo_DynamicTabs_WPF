c249c210282a956ae08dc1919405e479d0eb9184
Susan Dwyer <suzycream999@googlemail.com>
2024-06-18 16:47:07 +0100

    Add project files.

---
+++ HowTo_DynamicTabs_WPF/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace HowTo_DynamicTabs_WPF;
+
+public partial class MainWindow : Window
+{
+
+    List<int> _tabs = new List<int>();
+    int countIndex = 0;
+
+    public void AddNewTabItem()
+    {
+        int newIndex = countIndex++;
+
+        _tabs.Add(newIndex);
+
+        TabItem newTab = CreateNewTabItem(newIndex);
+        
+        tabControl.Items.Add(newTab);
+       
+        tabControl.SelectedIndex = newIndex;
+    }
+
+    private TabItem CreateNewTabItem(int newIndex)
+    {
+        var newTabHeader = new TabHeader(newIndex, $"Tab {newIndex}");
+        newTabHeader.TabCloseClicked += NewTabHeader_TabCloseClicked;
+        return new TabItem { Width = 100, Header = newTabHeader };
+    }
+
+    private void NewTabHeader_TabCloseClicked(object? sender, int e)
+    {
+        CloseTab(e);
+    }
+
+    public void CloseTab(int tabId)
+    {
+        tabControl.Items.RemoveAt(_tabs.IndexOf(tabId));
+        _tabs.Remove(tabId);
+        countIndex--;
+    }
+
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void Button_Click(object sender, RoutedEventArgs e)
+    {
+        AddNewTabItem();
+    }
+}
---
+++ HowTo_DynamicTabs_WPF/TabHeader.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace HowTo_DynamicTabs_WPF;
+/// <summary>
+/// Interaction logic for TabHeader.xaml
+/// </summary>
+public partial class TabHeader : UserControl
+{
+    public event EventHandler<int> TabCloseClicked;
+    int _index;
+
+    public TabHeader()
+    {
+        InitializeComponent();
+    }
+    public TabHeader(int index, string headerName)
+    {
+        InitializeComponent();
+        _index = index;
+        NameLabel.Content = headerName;
+    }
+
+    private void button_close_Click(object sender, RoutedEventArgs e)
+    {
+        TabCloseClicked?.Invoke(this, _index);
+    }
+}
